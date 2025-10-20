0888835b05ff1c3a18287106fb9689202b5128df
Megan Twyver <megan.twyver@seeq.com>
2025-10-20 07:14:17 -0600

    Part 1, Exercise 1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,42 +1,27 @@
 const Header = (props) => {
     console.log(props)
     return (
-    <div>
-            <h1> {props.course} </h1>
-    </div>
-    )
-}
-
-const Part = (props) => {
-    console.log(props)
-    return (
-        <div>
-            <p>
-                {props.part} {props.exercises}
-            </p>
-        </div>
+        <h1>
+            {props.course}
+        </h1>
     )
 }
 
 const Content = (props) => {
     console.log(props)
     return (
-        <div>
-            <Part part={props.part1} exercises={props.exercises1} />
-            <Part part={props.part2} exercises={props.exercises2} />
-            <Part part={props.part3} exercises={props.exercises3} />
-        </div>
+        <p>
+            {props.part} {props.exercises}
+        </p>
     )
 }
 
 const Total = (props) => {
     console.log(props)
     return (
-        <div>
-            <p>
-                Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
-            </p>
-        </div>
+        <p>
+            Total Exercises {props.total}
+        </p>
     )
 }
 
@@ -48,12 +33,14 @@ const App = () => {
     const exercises2 = 7
     const part3 = 'State of a component'
     const exercises3 = 14
-
+  
     return (
         <div>
             <Header course={course} />
-            <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+            <Content part={part1} exercises={exercises1} />
+            <Content part={part2} exercises={exercises2} />
+            <Content part={part3} exercises={exercises3} />
+            <Total total={exercises1 + exercises2 + exercises3} />
         </div>
     )
 }
