2506e569e482761c0b9282f8ec8494346ac3ff4f
Megan Twyver <megan.twyver@seeq.com>
2025-10-20 08:00:45 -0600

    Part 1, Exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -7,11 +7,23 @@ const Header = (props) => {
     )
 }
 
-const Content = (props) => {
+const Part = (props) => {
     console.log(props)
     return (
         <p>
             {props.part} {props.exercises}
+        </p>
+    )
+}
+
+const Content = (props) => {
+    console.log(props)
+
+    return (
+        <p>
+            <Part part={props.content[0].part} exercises={props.content[0].exercises} />
+            <Part part={props.content[1].part} exercises={props.content[1].exercises} />
+            <Part part={props.content[2].part} exercises={props.content[2].exercises} />
         </p>
     )
 }
@@ -33,13 +45,17 @@ const App = () => {
     const exercises2 = 7
     const part3 = 'State of a component'
     const exercises3 = 14
-  
+
+    const content = [
+        { part: part1, exercises: exercises1 },
+        { part: part2, exercises: exercises2 },
+        { part: part3, exercises: exercises3 }
+    ]
+
     return (
         <div>
             <Header course={course} />
-            <Content part={part1} exercises={exercises1} />
-            <Content part={part2} exercises={exercises2} />
-            <Content part={part3} exercises={exercises3} />
+            <Content content={content} />
             <Total total={exercises1 + exercises2 + exercises3} />
         </div>
     )
